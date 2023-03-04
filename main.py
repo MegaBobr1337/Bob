70dc3f03f31e1bfb2e9a2a7801db182bb5528bc5
MegaBobr1337 <126376539+MegaBobr1337@users.noreply.github.com>
2023-03-04 23:48:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ canvas.pack()
 
 frame = Frame(root)
 frame.place(relx=0,rely=0.3,relheight=0.7, relwidth=1)
-title = Label(frame, text="Нажмите на кнопку и говорите", font=("Arial",20))
+title = Label(frame, text="Нажмите на кнопку ", font=("Arial",20))
 title.pack()
 btn = Button(frame, text='Кнопка',bg='gray',width=15, height=3,font=("Arial",20))
 btn.pack(pady=50)
